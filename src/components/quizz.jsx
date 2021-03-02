7da4905ee7acdd448515dceddb3f77cade530152
Julien Bouland <bouland.julien@gmail.com>
2021-03-02 10:52:20 +0100

    TDD: task for quizz divided into actions

--- src/components/quizz.jsx
+++ src/components/quizz.jsx
@@ -9,7 +9,17 @@ class Quizz extends Component {
     }
     this.apiKey = process.env.TMDB_KEY
   }
-  
+
+  // fetch api
+  // get random actor
+  // get random movie
+  // ask question
+  // compare answer
+  // for each good answer add 10points
+  // handle next question
+  // after 5 questions stop game
+  // if 60seconds passed stop game
+  // display score and highscore
   render() {
     return(
       <div>
