4af33869e32f260192e0d7985f85640404c8fa5c
Julien Bouland <bouland.julien@gmail.com>
2021-03-02 16:39:29 +0100

    api: answer checked && buttons added

--- src/components/quizz.jsx
+++ src/components/quizz.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import 'regenerator-runtime/runtime';
+
 class Quizz extends Component {
   constructor(props) {
     super(props);
@@ -8,11 +10,12 @@ class Quizz extends Component {
       index: Math.floor(Math.random() * Math.floor(20)),
       movies: [],
       actors: [],
+      answer: null
     }
     this.apiKey = process.env.REACT_APP_TMDB_API,
     // this.apiKey = 'cc9affe1944340df2004885c27eab5e9',
     this.actorUrl = 'https://api.themoviedb.org/3/person/popular?api_key='
-    this.movieUrl = 'https://api.themoviedb.org/3/movie/popular?api_key='
+    this.movieUrl = 'https://api.themoviedb.org/3/movie/'
   }
 
   // fetch api
@@ -24,7 +27,7 @@ class Quizz extends Component {
       this.setState({
         // get array of actors
         actors: data.results,
-        // flat every movie from popular actors
+        // flat every famous movies from popular actors
         movies: data.results.map(result => result.known_for).flat(),
         isLoaded: true
       });
@@ -40,8 +43,28 @@ class Quizz extends Component {
     }
     )
   }
-  
+
   // compare answer
+  checkAnswer = async (movieId, actorId) => {
+    await fetch(`${this.movieUrl}${movieId}/credits?api_key=${this.apiKey}`)
+    .then(response => response.json())
+    .then((data) => {
+      if (data.cast.some(credit => credit.id === actorId)) {
+        this.setState({ answer: true});
+        console.log("RIGHT ANSWER");
+      } else {
+        this.setState({ answer: false});
+        console.log("FALSE ANSWER");
+      }
+    })
+  }
+  
+  handleAnswer = (boolean, movieId, actorId) => {
+    this.setState({
+      answer: boolean
+    });
+    this.checkAnswer(movieId, actorId);
+  }
   // for each good answer add 10points
   // handle next question
   // after 5 questions stop game
@@ -52,40 +75,26 @@ class Quizz extends Component {
   render() {
     
     const { actors, movies, index} = this.state;
-    // const fetchActors = (actors) => {
-      //   fetch(`${this.actorUrl}${this.apiKey}`)
-      //   .then(response => response.json())
-      //   .then(data =>{
-        //     this.setState({ 
-          //       isLoaded: true,
-          //       actors: data.results
-          //     })
-          //     // console.log(data.results);
-          //     // console.log(data.results.name);
-          //   })
-          // }
-          // fetchActors(actors);
-          // console.log(actors);
-          
+  
           let question;
-          let test;
           let actorImage;
           let movieImage;
+          let greenButton;
+          let redButton;
           if (this.state.isLoaded) {
-            // ask question
-            // question = <h2>Did {actors[index]['name']} played in Frozen ?</h2>
             question = <h2>Did {actors[index]['name']} played in { movies[index]['title'] } ?</h2>
             actorImage = <img src={`https://image.tmdb.org/t/p/w200` + actors[index]['profile_path']} alt={actors[index]['name']}/>
             movieImage = <img src={`https://image.tmdb.org/t/p/w200` + movies[index]['poster_path']} alt={movies[index]['title']}/>
-            test = <h2>hello from { console.log(movies) }</h2>
+            greenButton = <button src="assets/img/green_thumb.svg" onClick={this.handleAnswer(true, actors[index]['id'], movies[index]['id'])} alt="green thumb yes"></button>
+            redButton = <button src="assets/img/red_thumb.svg" onClick={this.handleAnswer(false, actors[index]['id'], movies[index]['id'])} alt="red thumb no"></button>
           }
-          // test2 = <p>{ console.log(actors) }</p>
           return(
             <div>
         { question }
-        { test }
         { actorImage }
         { movieImage }
+        { greenButton }
+        { redButton }
       </div>
     );
   }
