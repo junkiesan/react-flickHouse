47ab13e2202950b9245bee26097906c77f00a3e7
Julien Bouland <bouland.julien@gmail.com>
2021-03-03 11:58:57 +0100

    bug_fix: card width defined

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Welcome extends Component {
   render() {
     return (
-      <div className="welcome">
+      <div>
         <h1>Welcome to the quizz !</h1>
         <p>You'll be asked a series of "Yes or No" questions. Answer as many as you can in the allowed time ! </p>
         {/* <button className="welcome__btn" >Start</button> */}
