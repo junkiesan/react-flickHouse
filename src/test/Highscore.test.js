24bff3e87502e069425f389a54cf8daf6d11590d
Julien Bouland <bouland.julien@gmail.com>
2021-03-04 16:28:55 +0100

    test: draft made from PO user stories

---
+++ src/test/Highscore.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+
+import Quizz from '../components/quizz';
+
+describe('highscore', () => {
+  test('set a new highscore', () => {
+    // const { countdown } = gameOver({ time: 0 })
+    // gameOver({ resetButton: true  })
+    // expect(countdown).toEqual(0)
+  })
+  
+  test('doesnt change highscore cause current score is lower', () => {
+
+  })
+ 
+  test('highscore is displayed near score when superior to zero', () => {
+
+  })
+});
